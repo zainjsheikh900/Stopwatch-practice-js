7d1908166420526a16647cbb0d7c5f310228858f
Sheikh Zain Jaffer <136816448+zainjsheikh900@users.noreply.github.com>
2023-11-16 21:59:59 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+// setInterval (function(){
+//     document.write('Hello World')
+
+
+// },1000  )
+
+
+// function timer(){
+//     document.write("Zain")
+// }
+// setInterval(timer,4000)
+
+
+
+
+// setTimeout (function(){
+//     document.write('abcd')
+
+// },4000)
+
+
+
+
+// var num = 0
+// setInterval (function(){
+//     num++
+//     console.log(num)
+
+// },1000)
